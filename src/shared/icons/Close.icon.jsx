320b1316e7d040f41613fe7a9be780b95bcc02d2
RofaBR <rostislavbrigine@gmail.com>
2025-11-27 15:52:44 +0200

    style(icons) added new close icon

---
+++ src/shared/icons/Close.icon.jsx
@@ -0,0 +1,7 @@
+const CloseIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+        <path d="M18 6L6 18M6 6l12 12"></path>
+    </svg>
+);
+
+export default CloseIcon;
